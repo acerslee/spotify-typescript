9bae3af081a04c2ef0a2be6967a2bf56598d399b
Alex Lee <ahjlee712@gmail.com>
2021-05-08 16:48:25 -0400

    Rendered album image to feed

--- src/components/MusicDetail.tsx
+++ src/components/MusicDetail.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 interface Props{
-  albumImages: (string | number)[],
+  albumImages: any,
   albumName: string,
   artist: string,
   songName: string,
@@ -9,8 +9,24 @@ interface Props{
 }
 
 const MusicDetail: React.FC<Props> = ({ albumImages, albumName, artist, songName, uri}) => {
+
+  let renderSmallestImage = albumImages.reduce((smallest: {height: number}, image: {height: number}) => {
+    if (image.height < smallest.height) return image
+    return smallest;
+  })
+  // const [smallestImage, setSmallestImage] = useState('');
+
+  // useEffect(() => {
+  //   let smallest = albumImages[0];
+  //   if (smallest.height < )
+
+  // },[albumImages])
+
+  console.log('albumimage', renderSmallestImage)
+
   return(
     <div className = 'song'>
+      <img src = {renderSmallestImage.url} alt = 'Album cover' />
       <p>{albumName}</p>
       <p>{artist}</p>
       <p>{songName}</p>
