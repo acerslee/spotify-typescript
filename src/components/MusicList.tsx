6603afc8c628fdce641ee1a121f1b2ab0f2aa4ba
Alex Lee <ahjlee712@gmail.com>
2021-05-15 23:58:31 -0400

    Switched to table layout for list

--- src/components/MusicList.tsx
+++ src/components/MusicList.tsx
@@ -1,6 +1,31 @@
 import React from 'react';
 import MusicDetail from './MusicDetail';
 import styled from 'styled-components';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+
+interface Column {
+  id: 'index' | 'title' | 'artist' | 'album' | 'duration';
+  label: string;
+  minWidth?: number;
+  align?: 'right';
+  format?: (value: number) => string;
+}
+
+const columns: Column[] = [
+  { id: 'index', label: '#'},
+  { id: 'title', label: 'Title', minWidth: 100 },
+  { id: 'artist', label: 'Artist'},
+  { id: 'album', label: 'Album'},
+  { id: 'duration', label: 'Duration'}
+];
 
 interface Props {
   searchResults: (string | number)[],
@@ -19,10 +44,22 @@ const ListHeading = styled.h1`
   margin: 0.5rem 0.5rem;
 `;
 
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+  },
+  container: {
+    maxHeight: 440,
+  },
+});
+
 const MusicList: React.FC<Props> = ({ searchResults, retrieveSongData}) => {
 
+  const classes = useStyles();
+
   const dropdownSongData = (uri: string, artist: string, title: string) => {
-    retrieveSongData(uri, artist, title)
+    console.log(uri, artist, title);
+    // retrieveSongData(uri, artist, title)
   };
 
   return(
@@ -32,18 +69,46 @@ const MusicList: React.FC<Props> = ({ searchResults, retrieveSongData}) => {
           Search Results
         </ListHeading>
       }
-      {searchResults.map((song: any) => (
-        <MusicDetail
-          key = {song.uri}
-          albumImages = {song.album.images}
-          albumName = {song.album.name}
-          artist = {song.artists[0].name}
-          title = {song.name}
-          uri = {song.uri}
-          duration = {song.duration_ms}
-          dropdownSongData = {dropdownSongData}
-        />
-      ))}
+      <TableContainer className = {classes.container}>
+        <Table stickyHeader  aria-label = 'sticky table'>
+          <TableHead>
+            <TableRow>
+              {columns.map((column, index) => (
+                <TableCell
+                  key = {index}
+                  style = {{minWidth: column.minWidth}}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {searchResults.map((song: any, index: number) => {
+              return(
+                <TableRow key = {index} onClick = {() => dropdownSongData(song.uri, song.name,song.artists[0].name)}>
+                  <TableCell>{index + 1}</TableCell>
+                  <TableCell>{song.name}</TableCell>
+                  <TableCell>{song.artists[0].name}</TableCell>
+                  <TableCell>{song.album.name}</TableCell>
+                  <TableCell>{song.duration_ms}</TableCell>
+                </TableRow>
+              )
+              // <MusicDetail
+              //   key = {song.uri}
+              //   albumImages = {song.album.images}
+              //   albumName = {song.album.name}
+              //   artist = {song.artists[0].name}
+              //   title = {song.name}
+              //   uri = {song.uri}
+              //   duration = {song.duration_ms}
+              //   dropdownSongData = {dropdownSongData}
+              // />
+            })}
+          </TableBody>
+        </Table>
+
+      </TableContainer>
     </MusicListContainer>
   )
 };
